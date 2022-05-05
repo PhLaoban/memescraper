d261ddbd95e9ed52e680e2fd28e59709b5dc0ad0
Philipp Anton <philippanton140396@gmail.com>
2022-05-05 15:08:15 +0200

    commit pls

---
+++ index.js
@@ -0,0 +1,48 @@
+import fs, { mkdir } from 'node:fs';
+import https from 'node:https';
+import fetch from 'node-fetch';
+
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+const data = await response.text();
+
+let i;
+const urls = [];
+const str = data;
+const rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g;
+
+while ((i = rex.exec(str))) {
+  urls.push(i[1]);
+}
+
+const link =
+  'https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg?width=300';
+
+const urlcode = urls.slice(0, 10);
+
+let dir = './Memes/';
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir, { recursive: true });
+} else {
+  console.log('error');
+}
+
+console.log(urlcode);
+for (let n = 0; n < urlcode.length; n++) {
+  https
+    .get(urlcode[n], (res) => {
+      const imagePath = `./memes/0${n + 1}.jpg`;
+      const stream = fs.createWriteStream(imagePath);
+      res.pipe(stream);
+      stream.on('finish', () => {
+        stream.close();
+        console.log('Image downloaded');
+      });
+    })
+    .on('error', (err) => {
+      // handle error
+      console.log(err);
+    });
+}
